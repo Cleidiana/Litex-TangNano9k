df9e6214c9858e2103e816492fd6c61443f6c97b
Cleidiana <cleidiana.reis19@gmail.com>
2025-06-01 19:35:35 -0300

    ajuste repetição linux

--- teste_linux/main.c
+++ teste_linux/main.c
@@ -2,24 +2,29 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+//ajustar de acordo com teste
+#define INI_REP         5000*1000
+#define QUANT_REP       50000*1000
+
+
 static void teste_exec(void)
 {
 	uint32_t i;    
     puts("\nInicio teste\n");
     puts("\n3\n");
-    for (i = 0; i < 10000000; i++) {
+    for (i = 0; i < INI_REP; i++) {
         printf("."); 
     }
     puts("\n2\n");
-    for (i = 0; i < 10000000; i++) {
+    for (i = 0; i < INI_REP; i++) {
         printf("."); 
     }
     puts("\n1\n");
-    for (i = 0; i < 10000000; i++) {
+    for (i = 0; i < INI_REP; i++) {
         printf("."); 
     }
     puts("\nComecou\n");
-    for (i = 0; i < 100000000; i++) {
+    for (i = 0; i < QUANT_REP; i++) {
         printf("%" PRIu32 " ", i); 
     }
     puts("\nFim teste\n");
