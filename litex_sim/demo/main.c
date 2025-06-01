64a7db4abf20234ae88cd3558cf4d4b2ccdcfd87
Cleidiana <cleidiana.reis19@gmail.com>
2025-06-01 19:36:01 -0300

    repetição base simulação

--- litex_sim/demo/main.c
+++ litex_sim/demo/main.c
@@ -7,25 +7,28 @@
 #include <libbase/console.h>
 #include <generated/csr.h>
 
+//ajustar de acordo com teste
+#define INI_REP         5000
+#define QUANT_REP       50000
 
 static void teste_exec(void)
 {
    uint32_t i;   
    puts("\nInicio teste\n");
    puts("\n3\n");
-   for (i = 0; i < 1000; i++) {
+   for (i = 0; i < INI_REP; i++) {
        printf(".");
    }
    puts("\n2\n");
-   for (i = 0; i < 1000; i++) {
+   for (i = 0; i < INI_REP; i++) {
        printf(".");
    }
    puts("\n1\n");
-   for (i = 0; i < 1000; i++) {
+   for (i = 0; i < INI_REP; i++) {
        printf(".");
    }
    puts("\nComecou\n");
-   for (i = 0; i < 10000; i++) {
+   for (i = 0; i < QUANT_REP; i++) {
        printf("%" PRIu32 " ", i);
    }
    puts("\nFim teste\n");
@@ -34,13 +37,8 @@ static void teste_exec(void)
 
 int main(void)
 {
-
-
    uart_init();
-
-
    teste_exec();
-
 
    return 0;
 }
